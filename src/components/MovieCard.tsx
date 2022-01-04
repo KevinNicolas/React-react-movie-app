78bed42235b304eebf755fa8756b8aef381b104b
Kevin-Leguiza-Gaggero <nicolaskevinleguiza@gmail.com>
2022-01-04 15:28:10 -0300

    Make searchbar and add animations

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -14,8 +14,9 @@ export function MovieCard (props: props) {
     : noImagesUrl
 
   return (
-    <Link to={`/${props.movie.id}`} className="Movie-card-link">
+    <Link to={`/${props.movie.id}`} className="Movie-card-link fade-in">
       <div className="Movie-card">
+        {/* IMAGE */}
         <div className="image-container w-full h-full" style={{ background: `url("${imageURL}")`}}>
           <div 
             className={`
@@ -32,8 +33,21 @@ export function MovieCard (props: props) {
             <span>{ props.movie.vote_average }</span>
           </div>
         </div>
-        <div className="flex justify-center items-center w-full h-full px-2 text-center">
-          <span>{ props.movie.original_title }</span>
+        {/* TITLE */}
+        <div 
+          className="
+            flex 
+            justify-center items-center text-center 
+            w-full h-full
+            px-2
+            overflow-hidden overflow-ellipsis
+          "
+        >
+          <span 
+            className={`overflow-hidden overflow-ellipsis w-full ${ props.movie.original_title.length > 40 ? 'h-full' : '' }`}
+          >
+            { props.movie.original_title }
+          </span>
         </div>
       </div>
     </Link>
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, createRef, RefObject } from 'react';
 import { FiTriangle, FiSearch, FiLoader } from 'react-icons/fi'
 import debounce from 'lodash.debounce'
 
@@ -10,6 +10,7 @@ import { MovieCard } from '../components/MovieCard';
 import '../css/root.css'
 import '../css/general.css'
 
+import { useNavigate } from 'react-router'
 
 export function Root () {
 
@@ -17,25 +18,30 @@ export function Root () {
     movies: [] as T_movieData[],
     isLoading: true
   })
-
+  
   const [searchIsLoading, setSearchIsLoading] = useState(false)
 
   const queries = useQuery()
   const searchQuery: string | null = queries.get('search')
   const endpoint = searchQuery ? `/search/movie?query=${searchQuery}` : '/discover/movie'
-    
+  const navigate = useNavigate()
+  const textInput: RefObject<HTMLInputElement> = createRef<HTMLInputElement>()
   
   useEffect(() => {
+    if (textInput.current) { textInput.current.value = searchQuery ?? '' }
     httpClient.getMovies(endpoint)
       .then((data: any) => {
         const results: T_movieData[] = data.results
         setMovieData({ isLoading: false, movies: results })
+        setSearchIsLoading(false)
       })
-      .catch((err: any) => { console.error('Error:', err); throw err })
+      .catch((err: any) => { console.error('Error:', err); setSearchIsLoading(false); throw err })
   }, [searchQuery])
 
   function SearchMovie (event: any) {
     setSearchIsLoading(true)
+    navigate(`/?search=${event.target.value}`)
+    
   }
   const debouncedChangeHandler = useMemo(() => debounce(SearchMovie, 900), [])
 
@@ -46,12 +52,12 @@ export function Root () {
       <div className="main-view w-screen h-screen flex justify-center items-center">
         <div className="bg-black bg-opacity-30 flex flex-row justify-center items-center h-48 w-screen">
           <div>
-            <span className="text-blue-400 text-9xl">
+            <span className="text-blue-400 text-8xl sm:text-9xl">
               <FiTriangle />
             </span>
           </div>
           <div className="pb-3 flex flex-row items-end">
-            <span className="text-gray-200 text-7xl font-medium">React-movies</span>
+            <span className="text-gray-200 text-4xl sm:text-7xl font-medium">React-movies</span>
           </div>
         </div>
       </div>
@@ -60,10 +66,11 @@ export function Root () {
           <div className='bg-blue-400 pl-3 flex flex-row center gap-3 rounded-md text-white'>
               {
                 searchIsLoading
-                ? <FiLoader className='spin' size={28} />
-                : <FiSearch size={28} />
+                ? <FiLoader className='spin-fade-in' size={28} />
+                : <FiSearch className='fade-in' size={28} />
               }
             <input
+              ref={textInput}
               type="text"
               placeholder='Buscar...'
               className='text-black px-2 py-3 outline-none full'
