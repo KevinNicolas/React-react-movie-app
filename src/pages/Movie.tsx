dd2db771f676730c6d761cf2210f19899d499e21
Kevin-Leguiza-Gaggero <nicolaskevinleguiza@gmail.com>
2022-01-20 19:01:40 -0300

    Make responsive specific movie view

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -54,27 +54,27 @@ function ShowMovieInfo (props: { movie: detailedMovieData }) {
     return (
     <div className='movie-info-container full flex' style={backgroundStyles}>
       <div className="bg-black bg-opacity-80 text-white p-4 full flex flex-col py-4 gap-2">
-        <div className='flex justify-between items-center pl-2 pr-4'>
+        <div className='flex justify-between items-center pl-2 pr-4' style={{ minHeight: '5rem' }}>
             <span>
-              <span className='text-blue-400 text-4xl'>{ movie.title } </span>
-              <span className='text-gray-300 text-3xl'>( { movie.tagline } )</span>
+              <span className='text-blue-400 sm:text-4xl text-xl'>{ movie.title } </span>
+              { movie.tagline ? <span className='text-gray-300 sm:text-3xl text-lg'>( { movie.tagline } )</span> : <></> }
             </span>
           <div
             className={`
-            text-white rounded-full px-9 py-7 text-4xl flex flex-col
+            text-white rounded-full px-9 py-9 text-4xl hidden lg:flex flex-col
               ${getClassColor(movie.vote_average)}
             `}
           >
             <span>{ movie.vote_average }</span>
-            <span className='text-base'>of <span className='font-semibold text-purple-500'>{ movie.vote_count }</span></span>
+            <span className='text-sm'><span className='font-semibold text-base text-purple-500'>{ movie.vote_count }</span>.votes</span>
           </div>
         </div>
-        <div className='flex flex-col gap-2 px-8'>
+        <div className='flex flex-col gap-2 px-8' style={{ minHeight: '5rem' }}>
           <span>Popularity: <span className='font-semibold'>{ movie.popularity }</span></span>
           <span>Status: <span className='font-semibold'>{ movie.status }</span></span>
           <span>Release date: <span className='font-semibold'>{ movie.release_date }</span></span>
         </div>
-        <div className='px-6 py-2'>
+        <div className='overview px-6 py-2 border-dotted border-t-2 border-b-2 sm:border-t-0 sm:border-b-0 border-white'>
           <span className='text-lg'>{ movie.overview }</span>
         </div>
         <div className='py-4'>
@@ -90,7 +90,7 @@ function ShowMovieInfo (props: { movie: detailedMovieData }) {
       </div>
       <div className={`footer ${movie.production_companies.length > 1 ? 'justify-between' : 'justify-center'}`}>
         { movie.production_companies.map((company: company) => {
-          return (<img src={companyImageURL(company.logo_path)} alt={company.name} />)
+          return company.logo_path ? (<img src={companyImageURL(company.logo_path)} alt={company.name} />) : <></>
         }) }
       </div>
     </div>
